e11694ab6bccc138f1f85b4c7e444a2f7a4b8f1f
Roey Regev <roeyregev@MacBook-Pro-3.local>
2024-11-18 16:18:45 +0200

    drawer navigations, about page scss, images and videos integration

--- frontend/Models/project-model.ts
+++ frontend/Models/project-model.ts
@@ -1,10 +1,19 @@
+interface Image {
+    imageName: string;
+    imageDescription: string;
+  }
+  
+  interface Video {
+    videoLink: string;
+    videoDescription: string;
+  }
 class ProjectModel {
     public projectId!: number;
     public title!: string;
     public thumbnail!: string;
     public text!: string;
-    public images?: {}[];
-    public videos?: {}[];
+    public images?: Image[];
+    public videos?: Video[];
     public tags?: string[];
 }
 
--- frontend/components/Drawer/Drawer.tsx
+++ frontend/components/Drawer/Drawer.tsx
@@ -3,6 +3,7 @@ import styles from "./Drawer.module.scss";
 import IconClose from "../Icons/IconClose";
 import IconArrow from "../Icons/IconArrow";
 import ProjectModel from "@/Models/project-model";
+import { useState } from "react";
 
 interface DrawerProps {
     close: () => void;
@@ -12,12 +13,37 @@ interface DrawerProps {
 
 function Drawer({ close, selectedProjectIndex, projects }: DrawerProps): JSX.Element | null {
 
+    //find the relevant project by its id
+    const [selectedProject, setSelectedProject] = useState<ProjectModel | null>(projects.find(project => project.projectId === selectedProjectIndex) || null);
+    console.log("initial project:");
+    console.log(selectedProject);
+
     const portalRoot = document.getElementById("portal");
     if (!portalRoot) return null; // Don't render if the portal isn't available
 
-    //find the relevant project by its id
-    const selectedProject = projects.find(project => project.projectId === selectedProjectIndex);
-    console.log(selectedProject);
+
+    const nextProject = () => {
+        if (selectedProject?.projectId) {
+            const nextProject =
+                projects.find((project) => project.projectId === selectedProject.projectId + 1) ||
+                projects.find((project) => project.projectId === 1);
+            setSelectedProject(nextProject || null);
+        }
+    };
+
+    const previousProject = () => {
+        if (selectedProject?.projectId) {
+            const previousProject =
+                projects.find((project) => project.projectId === selectedProject.projectId - 1) ||
+                projects.find((project) => project.projectId === Math.max(...projects.map(p => p.projectId)));
+            setSelectedProject(previousProject || null);
+        }
+    };
+    
+
+
+
+
 
     return ReactDOM.createPortal(
         <>
@@ -26,8 +52,8 @@ function Drawer({ close, selectedProjectIndex, projects }: DrawerProps): JSX.Ele
                 <div className={styles.navIcons}>
                     <div className={styles.blankIcon}><p className={styles.blank}>blank</p></div>
                     <div className={styles.changePageFlex}>
-                        <div className={styles.previousIcon}><IconArrow /></div>
-                        <div className={styles.navIcon} ><IconArrow /></div>
+                        <div className={styles.previousIcon} onClick={previousProject}><IconArrow /></div>
+                        <div className={styles.navIcon} onClick={nextProject}><IconArrow /></div>
                     </div>
                     <div className={styles.navIcon} onClick={close}><IconClose /></div>
                 </div>
@@ -42,12 +68,36 @@ function Drawer({ close, selectedProjectIndex, projects }: DrawerProps): JSX.Ele
                         {selectedProject?.text}
                     </div>
 
+                    {/* images: */}
                     {selectedProject?.images && selectedProject?.images.length > 0 && (
                         <div className={styles.imagesContainer}>
                             {selectedProject?.images?.map((item) =>
-                            <div className={styles.box} key={selectedProject.projectId + "." + selectedProject.images?.indexOf(item)}>
-                             IMAGES 
-                            </div>)}
+                                <div className={styles.box} key={selectedProject.projectId + "." + selectedProject.images?.indexOf(item)}>
+                                    <img className={styles.projectImage} src={"/images/" + item.imageName} alt={item.imageName} />
+                                    <p className={styles.imageDescription}>{item.imageDescription}</p>
+                                </div>)}
+                        </div>
+                    )}
+
+                    {/* videos: */}
+                    {selectedProject?.videos && selectedProject?.videos.length > 0 && (
+                        <div className={styles.videoContainer}>
+                            {selectedProject?.videos?.map((item) =>
+                                <div className={styles.box} key={selectedProject.projectId + "." + selectedProject.videos?.indexOf(item)}>
+                                    <div className={styles.iframeContainer}>
+                                        <iframe
+                                            className={styles.projectVideo}
+                                            src={item.videoLink}
+                                            title={item.videoDescription}
+                                            // frameBorder="0"
+                                            // width={1920}
+                                            // height={1080}
+                                            // allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                            allowFullScreen
+                                        ></iframe>
+                                    </div>
+                                    <p className={styles.videoDescription}>{item.videoDescription}</p>
+                                </div>)}
                         </div>
                     )}
 
